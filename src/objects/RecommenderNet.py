5bade628d2b7e4dc52122b4d4263c25776fbd207
romain Boyrie <romain@boyrie.email>
2022-03-04 15:48:52 +0100

    Use of a Command line process to isolate prediction. Separation between Class and code.

---
+++ src/objects/RecommenderNet.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class RecommenderNet(keras.Model):
+    def __init__(self, num_users, num_books, embedding_size, **kwargs):
+        super(RecommenderNet, self).__init__(**kwargs)
+        self.num_users = num_users
+        self.num_books = num_books
+        self.embedding_size = embedding_size
+        self.user_embedding = layers.Embedding(
+            num_users,
+            embedding_size,
+            embeddings_initializer="he_normal",
+            embeddings_regularizer=keras.regularizers.l2(1e-6),
+        )
+        self.user_bias = layers.Embedding(num_users, 1)
+        self.book_embedding = layers.Embedding(
+            num_books,
+            embedding_size,
+            embeddings_initializer="he_normal",
+            embeddings_regularizer=keras.regularizers.l2(1e-6),
+        )
+        self.book_bias = layers.Embedding(num_books, 1)
+
+    def call(self, inputs):
+        user_vector = self.user_embedding(inputs[:, 0])
+        user_bias = self.user_bias(inputs[:, 0])
+        book_vector = self.book_embedding(inputs[:, 1])
+        book_bias = self.book_bias(inputs[:, 1])
+        dot_user_book = tf.tensordot(user_vector, book_vector, 2)
+        x = dot_user_book + user_bias + book_bias
+        return tf.nn.sigmoid(x)
