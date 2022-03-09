5fa3b35aaf14bdef8b2fa74b87bdbeca62bb4637
romain Boyrie <romain@boyrie.email>
2022-03-09 15:18:41 +0100

    bug of column selection

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -19,6 +19,8 @@ global clicks_agg  # Path du fichier concaténé
 global clicks_list
 global metadata_path
 global embeddings_path
+global client_by_book
+global book_by_client
 
 
 def load_params():
@@ -31,6 +33,8 @@ def load_params():
     global clicks_list
     global metadata_path
     global embeddings_path
+    global client_by_book
+    global book_by_client
 
     with open('params.yaml', 'r') as f:
         params = yaml.safe_load(f)
@@ -40,6 +44,8 @@ def load_params():
         clicks = str(params['data']['clicks'])
         clicks_agg = str(params['data']['clicks_agg'])
         book_db = str(params['data']['ranking'])
+        client_by_book = str(params['collab']['client_by_book'])
+        book_by_client = str(params['collab']['book_by_client'])
     clicks_list = glob(clicks + '/*.csv')
     clicks = choice(clicks_list)  # Sélection aléatoire d’un fichier de clicks
     return embeddings_path, metadata_path, matrix_size, clicks, clicks_list, clicks_agg, book_db
@@ -55,15 +61,29 @@ def build_db():
     global tfidf_matrix
     global clicks
     global clicks_list
+    global client_by_book
+    global book_by_client
+
     embeddings_path, metadata_path, matrix_size, clicks, clicks_list, clicks_agg, book_db = load_params()
     if not os.path.isfile(clicks_agg):
         usr_ds = pd.read_csv(clicks_list[0])[['user_id', 'click_article_id', 'session_size', 'click_timestamp']]
         for i in range(1, len(clicks_list)):
             usr_ds = pd.concat([usr_ds[['user_id', 'click_article_id', 'session_size', 'click_timestamp']],
-                                pd.read_csv(clicks_list[i])[['user_id', 'click_article_id', 'session_size', 'click_timestamp']]])
+                                pd.read_csv(clicks_list[i])[
+                                    ['user_id', 'click_article_id', 'session_size', 'click_timestamp']]])
             usr_ds.index.name = 'index'
-        list_usr = (usr_ds['user_id'].value_counts() > 4).index.tolist()
-        list_book = (usr_ds['click_article_id'].value_counts() > 1).index.tolist()
+        list_usr = usr_ds['user_id'].value_counts().reset_index().rename(columns=
+                                                                         {'user_id': 'book_count_usr',
+                                                                          'index': 'user_id'})
+        list_usr.index.name = 'index'
+        list_usr = list_usr.loc[list_usr['book_count_usr'] > book_by_client]['user_id'].to_list()
+
+        list_book = usr_ds['click_article_id'].value_counts().reset_index().rename(columns=
+                                                                                   {
+                                                                                       'click_article_id': 'click_count_book',
+                                                                                       'index': 'click_article_id'})
+        list_book.index.name = 'index'
+        list_book = list_book.loc[list_book['click_count_book'] > client_by_book]['click_article_id'].to_list()
         usr_ds = usr_ds.loc[usr_ds['user_id'].isin(list_usr)]  # Elimine les clients sans plus de 5 livres
         usr_ds = usr_ds.loc[usr_ds['click_article_id'].isin(list_book)]  # Elimine les livres sans plus d’un client
         usr_ds.to_csv(clicks_agg)
@@ -72,7 +92,8 @@ def build_db():
     if not os.path.isfile(book_db):
         usr_ds['rating'] = 1
 
-        usr_ds_ranking = usr_ds.groupby(['user_id', 'click_article_id']).agg({'click_timestamp': ['max'], 'rating': ['sum']})
+        usr_ds_ranking = usr_ds.groupby(['user_id', 'click_article_id']).agg(
+            {'click_timestamp': ['max'], 'rating': ['sum']})
         usr_ds_ranking = usr_ds_ranking.droplevel(level=1, axis=1).reset_index()
         usr_ds_ranking['rating'] = usr_ds_ranking['rating'].astype('float')
         usr_ds_ranking.index.name = 'index'
