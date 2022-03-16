3cdb14f91b766f4b8cb8546fcb343bab8974bb3a
Romain Boyrie <romain@boyrie.email>
2022-03-16 15:13:18 +0100

    Nommage des variables pour clarifier

--- src/ml_01.py
+++ src/ml_01.py
@@ -1,19 +1,15 @@
 import os
 import os.path as path
-from surprise import SVD
+import pandas as pd
 from surprise import accuracy
-from surprise import Dataset
-from surprise.model_selection import cross_validate
 from objects.BookLib import BookLib
 from surprise.model_selection import train_test_split
 from surprise import prediction_algorithms
 
-booklibrary = BookLib()
-data = booklibrary()
-trainset, testset = train_test_split(data, test_size=.25)
+book_library = BookLib()
+data = book_library()
+train_set, test_set = train_test_split(data, test_size=.25)
 
-
-# Then compute RMSE
 met = {'rmse': accuracy.rmse,
        'mae': accuracy.mae,
        'mse': accuracy.mse,
@@ -28,59 +24,60 @@ pred = {'NormalPred': ['metrics/normalPred', prediction_algorithms.random_pred.N
         'SVD': ['metrics/SVD', prediction_algorithms.matrix_factorization.SVD],
         'SVDpp': ['metrics/SVDpp', prediction_algorithms.matrix_factorization.SVDpp],
         'NMF': ['metrics/NMF', prediction_algorithms.matrix_factorization.NMF]
-}
+        }
 
-
-
-for a in pred.keys():
-    b = pred[a][0].split('/')
+for algo in pred.keys():
+    dir_list = pred[algo][0].split('/')
     algo = None
-    print(a)
+    print(algo)
     # We'll use the famous SVD algorithm.
-    if a == 'baseLineALS':
+    if algo == 'baseLineALS':
         bsl_options = {'method': 'als',
                        'n_epochs': 5,
                        'reg_u': 12,
                        'reg_i': 5
                        }
-        algo = pred[a][1](bsl_options=bsl_options)
-    elif a == 'baseLineSGD':
+        algo = pred[algo][1](bsl_options=bsl_options)
+    elif algo == 'baseLineSGD':
         bsl_options = {'method': 'sgd',
                        'learning_rate': .00005,
                        }
-        algo = pred[a][1](bsl_options=bsl_options)
-    elif a == 'KNNBasicALS':
+        algo = pred[algo][1](bsl_options=bsl_options)
+    elif algo == 'KNNBasicALS':
         bsl_options = {'method': 'als',
                        'n_epochs': 20,
                        }
         sim_options = {'name': 'pearson_baseline'}
-        algo = pred[a][1](bsl_options=bsl_options, sim_options=sim_options)
-    elif a == 'KNNBasicSimsCos':
+        algo = pred[algo][1](bsl_options=bsl_options, sim_options=sim_options)
+    elif algo == 'KNNBasicSimsCos':
         sim_options = {'name': 'cosine',
                        'user_based': False  # compute  similarities between items
                        }
-        algo = pred[a][1](sim_options=sim_options)
-    elif a == 'KNNBasicSimsMsd':
+        algo = pred[algo][1](sim_options=sim_options)
+    elif algo == 'KNNBasicSimsMsd':
         sim_options = {'name': 'msd',
                        'user_based': False  # compute  similarities between items
                        }
-        algo = pred[a][1](sim_options=sim_options)
-    elif a == 'KNNBasicPearson':
+        algo = pred[algo][1](sim_options=sim_options)
+    elif algo == 'KNNBasicPearson':
         sim_options = {'name': 'pearson'
                        }
-        algo = pred[a][1](sim_options=sim_options)
+        algo = pred[algo][1](sim_options=sim_options)
     else:
-        algo = pred[a][1]()
+        algo = pred[algo][1]()
 
     # Run 5-fold cross-validation and print results
-    algo.fit(trainset)
-    predictions = algo.test(testset)
+    algo.fit(train_set)
+    predictions = algo.test(test_set)
 
-    for it, dirs in enumerate(b):
-        if not path.isdir("/".join(b[:it + 1])):
-            os.makedirs("/".join(b[:it + 1]))
+    for it, dirs in enumerate(dir_list):
+        if not path.isdir("/".join(dir_list[:it + 1])):
+            os.makedirs("/".join(dir_list[:it + 1]))
 
-    c = ['rmse', 'mae', 'mse', 'fcp']
-    for met_name in c:
-        with open(pred[a][0] + '/' + met_name + '.txt', 'w') as f:
-            f.write(str(met[met_name](predictions)))
+    metric_list = ['rmse', 'mae', 'mse', 'fcp']
+    for met_name in metric_list:
+        with open(pred[algo][0] + '/' + met_name + '.txt', 'w') as f:
+            score = met[met_name](predictions)
+            f.write(str(score))
+            pd.DataFrame([[score]], columns=[met_name]) \
+                .to_csv(pred[algo][0] + "/" + met_name + ".tsv", index_label='index', sep='\t')
