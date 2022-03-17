91bed3beb303413efb7a368d0783ed9acbb833d6
Romain Boyrie <romain@boyrie.email>
2022-03-17 13:08:56 +0100

    deleting a comment

--- src/machine_learning.py
+++ src/machine_learning.py
@@ -7,17 +7,23 @@ from surprise.model_selection import train_test_split
 from surprise import prediction_algorithms
 from surprise.model_selection import LeaveOneOut
 from objects.Metrics import Metrics
+from surprise import KNNBaseline
 
 n = 10  # TopN
 book_library = BookLib()
 data, rankings = book_library()
+fulltrain = data.build_full_trainset()
 train_set, test_set = train_test_split(data, test_size=.25)
-LOOCV = LeaveOneOut(n_splits=1, random_state=1, min_n_ratings=20)
+LOOCV = LeaveOneOut(n_splits=1, random_state=42, min_n_ratings=3)
 for train, test in LOOCV.split(data):
     LOOCVTrain = train
     LOOCVTest = test
 
 LOOCVAntiTestSet = LOOCVTrain.build_anti_testset()
+# Compute similarty matrix between items so we can measure diversity
+sim_options = {'name': 'cosine', 'user_based': False}
+simsAlgo = KNNBaseline(sim_options=sim_options)
+simsAlgo.fit(fulltrain)
 
 met = {'rmse': accuracy.rmse,
        'mae': accuracy.mae,
@@ -26,26 +32,33 @@ met = {'rmse': accuracy.rmse,
        'TopN': Metrics.GetTopN,
        'HR': Metrics.HitRate,
        'cHR': Metrics.CumulativeHitRate,
-       'ARHR': Metrics.AverageReciprocalHitRank}
+       'ARHR': Metrics.AverageReciprocalHitRank,
+       'Coverage': Metrics.UserCoverage,
+       'Diversity': Metrics.Diversity,
+       'Novelty': Metrics.Novelty}
+
+
+
 pred = {'NormalPred': ['metrics/normalPred', prediction_algorithms.random_pred.NormalPredictor],
         'baseLineALS': ['metrics/baseLineALS', prediction_algorithms.baseline_only.BaselineOnly],
-        # 'baseLineSGD': ['metrics/baseLineSGD', prediction_algorithms.baseline_only.BaselineOnly],
-        # 'KNNBasicALS': ['metrics/knnBasicALS', prediction_algorithms.knns.KNNBasic],
-        # 'KNNBasicSimsCos': ['metrics/knnBasicSims', prediction_algorithms.knns.KNNBasic],
-        # 'KNNBasicSimsMsd': ['metrics/knnBasicSims', prediction_algorithms.knns.KNNBasic],
-        # 'KNNBasicPearson': ['metrics/knnBasicNoShrink', prediction_algorithms.knns.KNNBasic],
-        # 'SVD': ['metrics/SVD', prediction_algorithms.matrix_factorization.SVD],
-        # 'SVDpp': ['metrics/SVDpp', prediction_algorithms.matrix_factorization.SVDpp],
-        # 'NMF': ['metrics/NMF', prediction_algorithms.matrix_factorization.NMF]
+        'baseLineSGD': ['metrics/baseLineSGD', prediction_algorithms.baseline_only.BaselineOnly],
+        'KNNBasicALS': ['metrics/knnBasicALS', prediction_algorithms.knns.KNNBasic],
+        'KNNBasicSimsCos': ['metrics/knnBasicSims', prediction_algorithms.knns.KNNBasic],
+        'KNNBasicSimsMsd': ['metrics/knnBasicSims', prediction_algorithms.knns.KNNBasic],
+        'KNNBasicPearson': ['metrics/knnBasicNoShrink', prediction_algorithms.knns.KNNBasic],
+        'SVD': ['metrics/SVD', prediction_algorithms.matrix_factorization.SVD],
+        'SVDpp': ['metrics/SVDpp', prediction_algorithms.matrix_factorization.SVDpp],
+        'NMF': ['metrics/NMF', prediction_algorithms.matrix_factorization.NMF]
         }
 
 
-data = {'name': [], 'rmse': [], 'mae': [], 'mse': [], 'fcp': [], 'HR': [], 'cHR': [], 'ARHR': []}
+data = {'name': [], 'rmse': [], 'mae': [], 'mse': [], 'fcp': [], 'HR': [], 'cHR': [],
+        'ARHR': [], 'Coverage': [], 'Diversity': [], 'Novelty': []}
+
 for algo_name in pred.keys():
     dir_list = pred[algo_name][0].split('/')
     print(algo_name)
     algo = None
-    # We'll use the famous SVD algorithm.
     if algo_name == 'baseLineALS':
         bsl_options = {'method': 'als',
                        'n_epochs': 5,
@@ -85,18 +98,25 @@ for algo_name in pred.keys():
     predictions = algo.test(test_set)
     leftOutPredictions = algo.test(LOOCVTest)
     allPredictions = algo.test(LOOCVAntiTestSet)
+    topNPredicted = Metrics.GetTopN(allPredictions, n)
     for it, dirs in enumerate(dir_list):
         if not path.isdir("/".join(dir_list[:it + 1])):
             os.makedirs("/".join(dir_list[:it + 1]))
 
-    metric_list = ['rmse', 'mae', 'mse', 'fcp', 'HR', 'cHR', 'ARHR']
+    metric_list = ['rmse', 'mae', 'mse', 'fcp', 'HR', 'cHR', 'ARHR', 'Coverage', 'Diversity', 'Novelty']
 
-    topNPredicted = Metrics.GetTopN(allPredictions, n)
     for met_name in metric_list:
+        print(met_name)
         with open(pred[algo_name][0] + '/' + met_name + '.txt', 'w') as f:
             score = None
             if met_name in ['rmse', 'mae', 'mse', 'fcp']:
                 score = met[met_name](predictions)
+            elif met_name == 'Coverage':
+                score = met[met_name](topNPredicted, fulltrain.n_users, ratingThreshold=4.0)
+            elif met_name == 'Diversity':
+                score = met[met_name](topNPredicted, simsAlgo)
+            elif met_name == 'Novelty':
+                score = met[met_name](topNPredicted, rankings)
             else:
                 score = met[met_name](topNPredicted, leftOutPredictions)
             f.write(str(score))
