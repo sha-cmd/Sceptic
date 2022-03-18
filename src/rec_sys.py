7417acf0dcf0f65228b18f9eb7d1801a37c14f60
Romain Boyrie <romain@boyrie.email>
2022-03-18 17:34:19 +0100

    Travaux sur le calcul de nos recommendations.

--- src/rec_sys.py
+++ src/rec_sys.py
@@ -3,6 +3,7 @@ import os.path as path
 import pandas as pd
 import yaml
 from surprise import accuracy
+from joblib import Parallel, delayed
 from objects.BookLib import BookLib
 from surprise.model_selection import train_test_split
 from surprise import prediction_algorithms
@@ -12,6 +13,22 @@ from surprise import KNNBaseline
 from surprise.model_selection import KFold
 from pyparsing import Word, OneOrMore, Optional, Group, Combine, Suppress, alphanums, alphas, nums, srange
 
+global data_rs
+data_rs = {}
+
+
+def data_calc(raw_uid, raw_iid, algorithme):
+    global data_rs
+    digits = Word(nums)
+    event = "user: " + Word(alphanums) + "item: " + Word(alphanums) + "r_ui = " + Word(alphanums) + 'est = ' \
+            + Combine(digits + '.' + digits) + Group(OneOrMore(Word(srange("[{}]"))))
+    d = event.parseString(str(algorithme.predict(uid=raw_uid, iid=raw_iid)))
+    if not data.get(raw_uid) == None:
+        data_rs[raw_uid].update({d[3]: d[7]})
+    else:
+        data_rs.update({raw_uid: {d[3]: d[7]}})
+
+        
 with open('params.yaml', 'r') as f:
     params = yaml.safe_load(f)
 
@@ -34,7 +51,6 @@ fulltrain = data.build_full_trainset()
 topNPredicted = None
 train_set, test_set = train_test_split(data, test_size=0.025)
 
-
 pred = {'KNNBasicPearson': ['metrics/knnBasicNoShrink', prediction_algorithms.knns.KNNBasic]}
 for algo_name in pred.keys():
     algo = None
@@ -43,22 +59,8 @@ for algo_name in pred.keys():
         algo = pred[algo_name][1](sim_options=sim_options)
 
     algo.fit(fulltrain)
-    predictions = algo.predict(uid=user_encoded2user[123], iid=book_encoded2book[324])
-print(type(str(predictions)), str(predictions))
-t = str(predictions)
-data_usr = {}
-word = Word(alphanums)
-colon = Suppress(":")
-word = Word(alphanums)
-command = Group(OneOrMore(word))
-device = Group(OneOrMore(word))
-argument = Group(OneOrMore(word))
-digits = Word(nums).set_name("numeric digits")
-event = "user: " + Word(alphanums) + "item: " + Word(alphanums) + "r_ui = " + Word(alphanums) + 'est = ' \
-        + Combine(digits + '.' + digits) + Group(OneOrMore(Word(srange("[{}]"))))
-d = event.parseString(t)
-print(d)
-data_usr.update({d[3]: d[7]})
-print(data_usr)
-print(len(book_encoded2book))
-print(len(user_encoded2user))
+
+    # Calcul de la matrice des recommendations pour chaque couple user/item
+    for ruid in dataframe['userID'].unique():
+        Parallel(n_jobs=-1)(
+                delayed(data_calc)(ruid, riid, algo) for riid in dataframe['itemID'].unique())
