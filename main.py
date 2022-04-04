48dd16737303e31a023795ce180b204cb4d71c0c
Romain Boyrie <romain@boyrie.email>
2022-04-04 12:22:37 +0200

    Commentaires

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-"""Prototypage de fichier de fonction pour le déploiement"""
+"""Prototypage de fichier de fonction pour le déploiement, chaque fonction renvoyant un objet JSON contenant
+des recommandations d’articles pour un client en particulier, pour l’application responsive sha-cmd/bookshelf"""
 from src.objects.CF import CF
 from src.objects.CB import lambda_fct
 import json
@@ -9,6 +10,7 @@ from io import StringIO
 
  
 def inference_keras(req: functions.HttpRequest) -> str:
+    """Utilisation d’un neural-network avec Tensorflow via Keras"""
     parameters = req.get_json()
     user = parameters['userId']
     cf = CF()
@@ -16,6 +18,7 @@ def inference_keras(req: functions.HttpRequest) -> str:
 
 
 def inference_implicit(req: functions.HttpRequest) -> str:
+    """Utilisation de la bibliothèque Implicit"""
     parameters = req.get_json()
     user = parameters['userId']
     df = pd.read_csv('HttpTrigger/similar-artists.tsv', sep='\t', names=['userID', 'itemID', 'est'])
@@ -34,6 +37,7 @@ def inference_implicit(req: functions.HttpRequest) -> str:
 
 
 def inference_content_based(req: functions.HttpRequest) -> str:
+    """Utilisation des tenseurs et de la similarité du cosinus"""
     parameters = req.get_json()
     user = parameters['userId']
     return lambda_fct(user)
